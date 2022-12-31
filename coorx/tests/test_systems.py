97f81ed97b53e33bc3908edd25d2ed33f0d14a43
Luke Campagnola <luke.campagnola@gmail.com>
2022-12-30 18:23:19 -0800

    make exception tests more specific

--- coorx/tests/test_systems.py
+++ coorx/tests/test_systems.py
@@ -5,6 +5,11 @@ from coorx.systems import CoordinateSystemGraph
 from coorx.linear import STTransform
 
 
+missing_tr = "No transform defined linking"
+missing_cs = "No coordinate system named"
+wrong_ndim = r"is \dD \(expected \dD\)"
+wrong_system = "maps from system"
+
 def test_coordinate_systems():
     default_graph = CoordinateSystemGraph.get_graph(None)
     pt_cs1 = Point([0, 0], "cs1")
@@ -12,13 +17,13 @@ def test_coordinate_systems():
 
     assert pt_cs1.system is default_graph.system('cs1')
     assert np.all(pt_cs1.coordinates == [0, 0])
-    with raises(TypeError):
+    with raises(TypeError, match=missing_tr):
         pt_cs1.mapped_to('cs2')
-    with raises(NameError):
+    with raises(NameError, match=missing_cs):
         pt_cs1.mapped_to('nonexistent_cs')
-    with raises(TypeError):
+    with raises(TypeError, match=missing_tr):
         parr_cs2.mapped_to('cs2')
-    with raises(TypeError):
+    with raises(TypeError, match=wrong_ndim):
         Point([0, 0, 0], 'cs1')  # wrong ndim
  
     cs1_to_cs2 = STTransform(scale=[3, 2], offset=[10, 20], from_cs='cs1', to_cs='cs2')
@@ -29,9 +34,9 @@ def test_coordinate_systems():
 
     pt_cs2 = cs1_to_cs2.map(pt_cs1)
 
-    with raises(TypeError):
+    with raises(TypeError, match=wrong_system):
         cs1_to_cs2.map(pt_cs2)
-    with raises(TypeError):
+    with raises(TypeError, match=wrong_system):
         cs1_to_cs2.inverse.map(pt_cs1)
 
     assert pt_cs2.system is default_graph.system('cs2')
