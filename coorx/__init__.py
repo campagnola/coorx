0000055c52aa49f02de91f877c14495733d40a3f
Luke Campagnola <luke.campagnola@gmail.com>
2023-02-12 01:43:06 -0800

    fix create_transform

--- coorx/__init__.py
+++ coorx/__init__.py
@@ -18,7 +18,7 @@ def transform_types():
 
 
 _cached_types = None
-def create_transform(type, params):
+def create_transform(type, params, dims=None, systems=(None, None)):
     global _cached_types
     if _cached_types is None or type not in _cached_types:
         _cached_types = {tr.__name__: tr for tr in transform_types()}
@@ -26,4 +26,4 @@ def create_transform(type, params):
     if type not in _cached_types:
         raise TypeError('Unknown transform type %r' % type)
 
-    return _cached_types[type](**params)
+    return _cached_types[type](dims=dims, from_cs=systems[0], to_cs=systems[1], **params)
