6d77c8769bfab2ea7258b2d93957b4910a0f3853
Martin Chase <outofculture@gmail.com>
2024-12-26 11:35:18 -0800

    whitespace

--- coorx/composite.py
+++ coorx/composite.py
@@ -14,6 +14,7 @@ class CompositeTransform(Transform):
     transforms : list of Transform instances
         See ``transforms`` property.
     """
+
     Linear = False
     Orthogonal = False
     NonScaling = False
@@ -24,7 +25,7 @@ class CompositeTransform(Transform):
         self._transforms = []
         self._simplified = None
         self._null_transform = NullTransform()
-        
+
         # Set input transforms
         trs = []
         for tr in transforms:
@@ -33,9 +34,11 @@ class CompositeTransform(Transform):
             else:
                 trs.append(tr)
         self.transforms = trs
-        for i in range(len(trs)-1):
-            if trs[i].systems[1] != trs[i+1].systems[0]:
-                raise TypeError(f"Coordinate systems of transform {trs[i]} ({trs[i].systems[1]}) does not map to {trs[i+1]} ({trs[i+1].systems[0]})")
+        for i in range(len(trs) - 1):
+            if trs[i].systems[1] != trs[i + 1].systems[0]:
+                raise TypeError(
+                    f"Coordinate systems of transform {trs[i]} ({trs[i].systems[1]}) does not map to {trs[i+1]} ({trs[i+1].systems[0]})"
+                )
 
     @property
     def systems(self):
@@ -49,23 +52,23 @@ class CompositeTransform(Transform):
 
     @property
     def transforms(self):
-        """ The list of transform that make up the transform chain.
-        
+        """The list of transform that make up the transform chain.
+
         The order of transforms is given such that the first transform in the
-        list is the first to be invoked when mapping coordinates through 
-        the chain. 
-        
+        list is the first to be invoked when mapping coordinates through
+        the chain.
+
         For example, the following two mappings are equivalent::
-        
+
             # Map coordinates through individual transforms:
             trans1 = STTransform(scale=(2, 3), translate=(0, 1))
             trans2 = PolarTransform()
             mapped = trans2.map(trans1.map(coords))
-            
+
             # Equivalent mapping through chain:
             chain = CompositeTransform([trans1, trans2])
             mapped = chain.map(coords)
-            
+
         """
         return self._transforms
 
@@ -75,17 +78,13 @@ class CompositeTransform(Transform):
             tr = [tr]
         if not isinstance(tr, list):
             raise TypeError("Transform chain must be a list")
-        
+
         # Avoid extra effort if we already have the correct chain
         if len(tr) == len(self._transforms):
-            changed = False
-            for i in range(len(tr)):
-                if tr[i] is not self._transforms[i]:
-                    changed = True
-                    break
+            changed = any(tr[i] is not self._transforms[i] for i in range(len(tr)))
             if not changed:
                 return
-        
+
         for t in self._transforms:
             t.remove_change_callback(self._subtr_changed)
         self._transforms = tr
@@ -95,8 +94,7 @@ class CompositeTransform(Transform):
 
     @property
     def simplified(self):
-        """A simplified representation of the same transformation.
-        """
+        """A simplified representation of the same transformation."""
         if self._simplified is None:
             self._simplified = SimplifiedCompositeTransform(self)
         return self._simplified
@@ -129,7 +127,7 @@ class CompositeTransform(Transform):
             b &= tr.Isometric
         return b
 
-    def map(self, obj:Mappable):
+    def map(self, obj: Mappable):
         return self._map(obj)
 
     def _map(self, coords):
@@ -217,8 +215,7 @@ class CompositeTransform(Transform):
         self._update()
 
     def _subtr_changed(self, event):
-        """One of the internal transforms changed; propagate the signal. 
-        """
+        """One of the internal transforms changed; propagate the signal."""
         self._update(event)
 
     def __setitem__(self, index, tr):
@@ -244,12 +241,12 @@ class CompositeTransform(Transform):
     def __eq__(self, b):
         if not isinstance(b, CompositeTransform):
             return False
-        return all([t1==t2 for t1,t2 in zip(self.transforms, b.transforms)])
+        return all(t1 == t2 for t1, t2 in zip(self.transforms, b.transforms))
 
     def __str__(self):
         names = [tr.__class__.__name__ for tr in self.transforms]
         return f"<{self.__class__.__name__} [{', '.join(names)}] at 0x{id(self):x}>"
-    
+
     def __repr__(self):
         tr = ",\n                 ".join(map(repr, self.transforms))
         return f"<{self.__class__.__name__} [{tr}] at 0x{id(self):x}>"
@@ -263,14 +260,13 @@ class SimplifiedCompositeTransform(CompositeTransform):
         self.source_changed(None)
 
     def source_changed(self, event):
-        """Generate a simplified chain by joining adjacent transforms.
-        """
+        """Generate a simplified chain by joining adjacent transforms."""
         # bail out early if the chain is empty
         transforms = self._chain.transforms[:]
         if len(transforms) == 0:
             self.transforms = []
             return
-        
+
         # If the change signal comes from a transform that already appears in
         # our simplified transform list, then there is no need to re-simplify.
         if event is not None:
@@ -278,7 +274,7 @@ class SimplifiedCompositeTransform(CompositeTransform):
                 if source in self.transforms:
                     self._update(event)
                     return
-        
+
         # First flatten the chain by expanding all nested chains
         new_chain = []
         while len(transforms) > 0:
@@ -287,7 +283,7 @@ class SimplifiedCompositeTransform(CompositeTransform):
                 transforms = tr.transforms[:] + transforms
             else:
                 new_chain.append(tr)
-        
+
         # Now combine together all compatible adjacent transforms
         cont = True
         tr = new_chain
@@ -297,8 +293,7 @@ class SimplifiedCompositeTransform(CompositeTransform):
             for t2 in tr[1:]:
                 t1 = new_tr[-1]
                 pr = t2 * t1
-                if (not t1.dynamic and not t2.dynamic and not 
-                   isinstance(pr, CompositeTransform)):
+                if not t1.dynamic and not t2.dynamic and not isinstance(pr, CompositeTransform):
                     cont = True
                     new_tr.pop()
                     new_tr.append(pr)
