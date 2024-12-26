ce2a41c6d11e9263b4bdb285da8ed66944efa4ab
Martin Chase <outofculture@gmail.com>
2024-12-26 14:28:24 -0800

    get the version from the source

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 from setuptools import setup, find_packages
 
 packages = [x for x in find_packages(".") if x.startswith("coorx")]
@@ -6,9 +8,19 @@ packages = [x for x in find_packages(".") if x.startswith("coorx")]
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+def get_version():
+    init_py = os.path.join(os.path.dirname(__file__), "coorx", "__init__.py")
+    with open(init_py, "r") as f:
+        content = f.read()
+    if version_match := re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M):
+        return version_match[1]
+    raise RuntimeError("Unable to find version string")
+
+
 setup(
     name="coorx",
-    version="1.0.2",
+    version=get_version(),
     author="Luke Campagnola",
     author_email="lukec@alleninstitute.org",
     description="A dynamic web of linear and nonlinear coordinate system transforms.",
