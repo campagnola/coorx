6d92adc6fc2465f31159159d80ee4920fcecd36a
Luke Campagnola <luke.campagnola@gmail.com>
2026-01-07 14:51:50 -0800

    dont allow duplicate callbacks

--- coorx/events.py
+++ coorx/events.py
@@ -20,44 +20,52 @@ class ChangeEvent:
 
 
 class CallbackRegistry:
+    """A thread-safe registry for callbacks, supporting weak references."""
     def __init__(self):
         # List of (is_weakref, callback or weakref) tuples
         self._callbacks: list[tuple[bool, Callable]] = []
         self.lock = threading.Lock()
 
+    def _real_callbacks(self):
+        """Return (callback, is_weakref, maybe_weakref) tuples for all live callbacks."""
+        return [(cb, is_ref, cb_ref) for is_ref, cb_ref in self._callbacks if (cb := (cb_ref() if is_ref else cb_ref)) is not None]
+
     def add(self, cb, keep_reference):
+        """Register a callback.
+        If keep_reference is False, a weak reference to the callback is stored,
+        allowing it to be garbage collected if there are no other references.
+        """
         if keep_reference:
             cb_ref = (False, cb)
         else:
+            # Create a cleanup call back so dead weakrefs are removed from the registry
             weak_self = weakref.ref(self)
-
             def cleanup(dead_ref):
                 registry = weak_self()
                 if registry is not None:
                     registry.remove(dead_ref)
 
+            # create weak reference to the callback
             if inspect.ismethod(cb):
                 cb_ref = (True, weakref.WeakMethod(cb, cleanup))
             else:
                 cb_ref = (True, weakref.ref(cb, cleanup))
 
         with self.lock:
+            # Prevent duplicate registrations
+            for other_cb, _, _ in self._real_callbacks():
+                if other_cb == cb:
+                    raise ValueError("Callback already registered")
+            
+            # Add the new callback
             self._callbacks.append(cb_ref)
 
     def remove(self, cb):
         with self.lock:
             new_callbacks = []
-            for is_ref, maybe_cb in self._callbacks:
-                if is_ref:
-                    cb_from_ref = maybe_cb()
-                    if cb_from_ref is None:
-                        # Clean up dead weak refs, too
-                        continue
-                    if cb_from_ref == cb:
-                        continue
-                else:
-                    if maybe_cb == cb:
-                        continue
+            for other_cb, is_ref, maybe_cb in self._real_callbacks():
+                if other_cb == cb:
+                    continue
                 new_callbacks.append((is_ref, maybe_cb))
             self._callbacks = new_callbacks
 
@@ -65,7 +73,7 @@ class CallbackRegistry:
         """Invoke all registered callbacks with the given arguments."""
         with self.lock:
             # Make a snapshot of callbacks to invoke
-            callbacks = [cb_ref() if is_ref else cb_ref for is_ref, cb_ref in self._callbacks]
-        for cb in callbacks:
+            callbacks = self._real_callbacks()
+        for cb, _, _ in callbacks:
             if cb is not None:
                 cb(*args, **kwargs)
